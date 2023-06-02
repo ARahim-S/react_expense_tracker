2b978c3f33435c6d6c6dab729b3bf4f1c6b9eb39
ARahim-S <arahim.s@yandex.com>
2023-06-02 20:08:12 +0300

    v3

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 export const ExpenseForm = () => {
+  const [input, setInput] = useState({});
+  const changeHandler = (e) => {
+    setInput((prevState) => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" name="title" onChange={changeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" max="0.01" />
+          <input
+            type="number"
+            name="number"
+            min="0.01"
+            step="0.01"
+            onChange={changeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2023-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2023-12-31"
+            name="date"
+            onChange={changeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
